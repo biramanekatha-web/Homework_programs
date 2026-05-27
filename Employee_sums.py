fb9e565de8a9beb61aaa19fb2e6a9ad7bd8af1e6
kathabiramane <biramanekatha@gmail.com>
2026-05-27 14:52:31 +0530

    Initial commit

---
+++ Employee_sums.py
@@ -0,0 +1,50 @@
+# list1 = [1,2,3,4,5]
+# sum=0
+# for i in list1:
+#     sum=sum+i
+# print("Sum of list is ",sum)
+
+
+salary = [67000,45000,78000,55000,28000,44000,33000]
+# num1=salary.sort()
+num2=sorted(salary)
+print(num2)
+print(salary)
+# print("Sorted list is ",salary)
+# print(salary[-2])
+# print(salary[1])
+# rev=" "
+# for numbers in salary:
+#     rev=str(numbers)+" "+rev
+# print(rev)
+
+# for num in salary:
+
+#     count = 0
+
+#     for num2 in salary:
+
+#         if num2 > num:
+#             count = count + 1
+
+#     if count == 1:
+#         print("Second Maximum =", num)
+
+
+# salary = [67000,45000,78000,55000,28000,44000,33000]
+
+# for num in salary:
+
+#     count = 0
+
+#     for num2 in salary:
+
+#         if num2 < num:
+#             count = count + 1
+
+#     if count == 1:
+#         print("Second Minimum =", num)
+
+
+     
+     
